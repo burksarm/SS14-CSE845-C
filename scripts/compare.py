4e07325bcc5490f519f3fede81060f067ee52ba6
Armand Burks <burksarm@cse.msu.edu>
2014-02-09 21:29:13 -0500

    Finished compare script and generated competition events files and environment files.

--- scripts/compare.py
+++ scripts/compare.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python
 import re
+import environments 
+#Handles generating environments for the competitions
 
 #Compares each A/B pair to find pairs which A has a replication rate of at least 1.5 greater than B.
 #A is from the low mutation rate, B is from the high mutation rate.
 
 #Go through all the pairs and get their fitness
 for i in range(1, 41):
-	aFitness = float(0)
-	bFitness = float(0)
-	aViable = "0"
-	bViable = "0"
+	aFitness = float(0) #Will hold organism A's fitness
+	bFitness = float(0) #Will hold organism B's fitness
+	aViable = "0"       #Whether or not organism A is viable
+	bViable = "0"       #Whether or not organism B is viable
+	aMerit  =  None     #Holds organism A's merit
+	bMeriit =  None     #Holds organism B's merit
 	
 	aFile = open("../organisms/flatPool/dom-%s.org-A" %i, "r")
 	bFile = open("../organisms/flatPool/dom-%s.org-B" %i, "r")
@@ -19,6 +23,10 @@ for i in range(1, 41):
 		if line.startswith("# Is Viable"):
 			aViable = line.split()[-1]
 		
+		#Get the merit
+		if line.startswith("# Merit"):
+			aMerit = line.split()[-1]
+		
 		if line.startswith("# Fitness"):
 			aFitness = float(line.split()[2])
 			break
@@ -26,12 +34,39 @@ for i in range(1, 41):
 	for line in bFile:
 		if line.startswith("# Is Viable"):
 			bViable = line.split()[-1]
+			
+		#Get the merit
+		if line.startswith("# Merit"):
+			bMerit = line.split()[-1]
 		
 		if line.startswith("# Fitness"):
 			bFitness = float(line.split()[2])
 			break
 		
-	#Now compare them
+	#Now compare them and if they meet the criteria, generate an events file for their competition
 	if aViable == "1" and bViable == "1" and aFitness/bFitness > 1.5:
-		print aFile.name
-		print bFile.name
+		#print aFile.name
+		#print bFile.name
+		
+		#Create the events file
+		eventsFile = open("../avidaFiles/events_dom-%s_comp.cfg" %i, "w")
+		
+		#Fill half the pop with organism A and give it a marker of 0
+		eventsFile.write("#Fill half the pop with organism A and give it a marker of 0\n")
+		eventsFile.write("u begin InjectRange %s 0 1799 %s 0\n\n" %(aFile.name, aMerit))
+		
+		#Fill half the pop with organism B and give it a marker of 1
+		eventsFile.write("#Fill half the pop with organism B and give it a marker of 1\n")
+		eventsFile.write("u begin InjectRange %s 1800 3599 %s 0\n\n" %(bFile.name, bMerit))
+		
+		#Save the population after 50 generations
+		eventsFile.write("#Save the population after 50 generations\n")
+		eventsFile.write("g 50 SavePopulation\n\n")
+		
+		#Stop after 50 generations
+		eventsFile.write("#Stop after 50 generations\n")
+		eventsFile.write("g 50 Exit\n")
+		
+		#Now we need to generate an environment file to only reward tasks either A or B performs
+		environments.generate(aFile.name, bFile.name)
+	
---
+++ scripts/environments.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+import os
+
+#task names to match in the organism file
+tasks = ["not", "nand", "and", "orn", "or", "andn", "nor", "xor", "equ"]
+
+#Gets the set of tasks performed in the given organism file
+def getTasks(file):
+	tasksPerformed = set()
+	for line in file:
+		#Get the tasks performed line
+		items = line.split()
+
+		if len(items) >= 4 and items[1] in tasks:
+			#Gather the task if it was performed
+			if int(items[2]) > 0:
+				tasksPerformed.add(items[1])
+	
+	return tasksPerformed
+
+#Generates the environment file that rewards the combined set of tasks A and B perform
+def generate(aFile, bFile):
+	#Will hold all the resource strings we'll write to file
+	allResources = {} #format: task -> original line of text
+
+	#Steal the resource strings from the regular environment.cfg file
+	envFile = open("../avidaFiles/environment.cfg", "r")
+
+	for line in envFile:
+		if line.startswith("REACTION"):
+			items = line.split()
+			allResources[items[2]] = line.strip()
+
+
+	#Get the set of tasks performed by both A and B
+	allTasks = getTasks(open(aFile, "r"))
+	allTasks.union(getTasks(open(bFile, "r")))
+	
+	#Get the dominant ancestor name that A and B are based on
+	domName = aFile[aFile.rfind("/")+1:aFile.rfind(".")]
+	
+	#Now write the environment file
+	outFile = open("../avidaFiles/environment_%s_comp.cfg" %domName, "w")
+	for task in allTasks:
+		outFile.write(allResources[task] + "\n")
+	
+	if len(allTasks) < 9:
+		print domName
+	
+
+
